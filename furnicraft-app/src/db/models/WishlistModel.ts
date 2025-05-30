e716c734c4cbadf68ed6c9577756db8196e4acd8
mfaishaldp <faishaldarmaputra@gmail.com>
2025-05-30 14:10:24 +0700

    feat/wishlist

---
+++ furnicraft-app/src/db/models/WishlistModel.ts
@@ -0,0 +1,47 @@
+import { ObjectId } from "mongodb";
+import { db } from "../config/mongodb";
+
+class WishlistModel {
+    static collection () {
+        return db.collection('Wishlist')
+    }
+    static async addWishlist ({UserId, ProductId} : {UserId : string, ProductId : string}) {
+        return await this.collection().insertOne({UserId : new ObjectId(UserId), ProductId : new ObjectId(ProductId)});
+    }
+    static async getWishlist () {
+        return await this.collection().aggregate(
+            [
+                {
+                    '$lookup': {
+                    'from': 'Products', 
+                    'localField': 'ProductId', 
+                    'foreignField': '_id', 
+                    'as': 'DetailProduct'
+                    }
+                }, {
+                    '$unwind': {
+                    'path': '$DetailProduct', 
+                    'preserveNullAndEmptyArrays': true
+                    }
+                }
+            ]
+        ).toArray()
+    }
+    static async getWishlistByUserIdProductId (UserId : string, ProductId : string) {
+        return await this.collection().findOne({
+            $and : [
+                {
+                    UserId : new ObjectId(UserId)
+                },
+                {
+                    ProductId : new ObjectId(ProductId)
+                }
+            ]
+        })
+    }
+    static async delWishlist(id : string) {
+        return this.collection().deleteOne({_id : new ObjectId(id)})
+    }
+}
+
+export default WishlistModel
